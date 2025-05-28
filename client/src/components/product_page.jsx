78560eb2b22ae614f668a804b4a6c7ddd9950c6f
Sagnik84 <sagnikhalder10@gmail.com>
2025-05-28 17:24:57 +0530

    updated product page

--- client/src/components/product_page.jsx
+++ client/src/components/product_page.jsx
@@ -29,11 +29,8 @@ const ProductPage = () => {
         console.log("Fetching listing for ID:", id);
         console.log("Fetching listing for ID:", id);
         const response = await axios.get(
-<<<<<<< HEAD
           `http://localhost:3000/api/product/get/${id}`
-=======
-          `http://localhost:3000/api/product/get/${params.id}`
->>>>>>> babca0be4e054ab5e3299a313f4db812062e17bb
+
         );
         setListing(response.data); // ✅ Axios response data
         console.log(response.data);
